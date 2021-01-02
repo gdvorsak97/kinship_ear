7167e2f32a42d48feb4baccd12ef1c57adad2cc4
Grega Dvoršak <gdvorsak97@gmail.com>
2021-01-02 01:42:49 +0100

    Notes created, next use checkpoints to see if all train/test images are in, also recreate the sample submission

--- demo1_FIW_DL.py
+++ demo1_FIW_DL.py
@@ -13,54 +13,68 @@ from keras.optimizers import Adam
 from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
 
-train_file_path = "../input/train_relationships.csv"
-train_folders_path = "../input/train/"
-val_famillies = "F09"
+train_file_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/train_list.csv"
+train_folders_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/"
+# this F09 is a family folder one of many.. check why only one
+val_famillies = "F09"  # validation images, list families to separate train, test
 
-all_images = glob(train_folders_path + "*/*/*.jpg")
-train_images = [x for x in all_images if val_famillies not in x]
-val_images = [x for x in all_images if val_famillies in x]
+all_images = glob(train_folders_path + "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/*/*/*.jpg")
+train_images = [x for x in all_images if val_famillies not in x]  # all except families in val_families
+val_images = [x for x in all_images if val_famillies in x]  # all images not in validation set
 
+# dictionary for a map of person:image
 train_person_to_images_map = defaultdict(list)
-ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
+ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]  # family/person
 for x in train_images:
+    # append train image to the person in the above format
     train_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
 
+# similar but validation
 val_person_to_images_map = defaultdict(list)
 for x in val_images:
+    # same as above but val images
     val_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
 
+# get pairs from csv to a zipped list
 relationships = pd.read_csv(train_file_path)
 relationships = list(zip(relationships.p1.values, relationships.p2.values))
+# validate for people - check after if all accounted for
 relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
 
+# get train and val relationship list
 train = [x for x in relationships if val_famillies not in x[0]]
 val = [x for x in relationships if val_famillies in x[0]]
 
 
+# read images
 def read_img(path):
     img = cv2.imread(path)
     img = np.array(img).astype(np.float)
     return preprocess_input(img, version=2)
 
 
+# generator of labels for pairs
 def gen(list_tuples, person_to_images_map, batch_size=16):
     ppl = list(person_to_images_map.keys())
     while True:
-        batch_tuples = sample(list_tuples, batch_size // 2)
+        batch_tuples = sample(list_tuples, batch_size // 2) # random tuples
         labels = [1] * len(batch_tuples)
         while len(batch_tuples) < batch_size:
+            # choose 2 people
             p1 = choice(ppl)
             p2 = choice(ppl)
 
+            # if not in random tuples, add it there and append a zero label
             if p1 != p2 and (p1, p2) not in list_tuples and (p2, p1) not in list_tuples:
                 batch_tuples.append((p1, p2))
                 labels.append(0)
 
+        # A check if all exist
         for x in batch_tuples:
             if not len(person_to_images_map[x[0]]):
                 print(x[0])
 
+        # images loaded from random choices, labels added
         X1 = [choice(person_to_images_map[x[0]]) for x in batch_tuples]
         X1 = np.array([read_img(x) for x in X1])
 
@@ -70,6 +84,7 @@ def gen(list_tuples, person_to_images_map, batch_size=16):
         yield [X1, X2], labels
 
 
+# Straightforward, generate model as described in the post
 def baseline_model():
     input_1 = Input(shape=(224, 224, 3))
     input_2 = Input(shape=(224, 224, 3))
@@ -108,12 +123,15 @@ def baseline_model():
 
 file_path = "vgg_face.h5"
 
+# callback to save weights
 checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 
+# reduce rate when learning stagnates
 reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.1, patience=20, verbose=1)
 
 callbacks_list = [checkpoint, reduce_on_plateau]
 
+# can load weights below
 model = baseline_model()
 # model.load_weights(file_path)
 model.fit_generator(gen(train, train_person_to_images_map, batch_size=16), use_multiprocessing=True,
@@ -127,10 +145,12 @@ def chunker(seq, size=32):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
+# have to recreate this file
 submission = pd.read_csv('../input/sample_submission.csv')
 
 predictions = []
 
+# get predictions
 for batch in tqdm(chunker(submission.img_pair.values)):
     X1 = [x.split("-")[0] for x in batch]
     X1 = np.array([read_img(test_path + x) for x in X1])
@@ -141,6 +161,7 @@ for batch in tqdm(chunker(submission.img_pair.values)):
     pred = model.predict([X1, X2]).ravel().tolist()
     predictions += pred
 
+# output
 submission['is_related'] = predictions
 
 submission.to_csv("vgg_face.csv", index=False)
