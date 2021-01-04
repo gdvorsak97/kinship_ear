149ab0cb6ad8a526fe9560fcb3e015c370d15932
Grega Dvoršak <gdvorsak97@gmail.com>
2021-01-04 20:32:55 +0100

    Set up until training phase, hopefully it works as expected, still no sample submission file though

--- demo1_FIW_DL.py
+++ demo1_FIW_DL.py
@@ -6,16 +6,16 @@ import numpy as np
 from random import choice, sample
 from tqdm import tqdm
 
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
-from keras.layers import Input, Dense, GlobalMaxPool2D, GlobalAvgPool2D, Concatenate, Multiply, Dropout, Subtract
-from keras.models import Model
-from keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.layers import Input, Dense, GlobalMaxPool2D, GlobalAvgPool2D, Concatenate, Multiply, Dropout, \
+    Subtract
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
 
 train_file_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/train_list.csv"
 train_folders_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/"
-# this F09 is a family folder one of many.. check why only one
 val_famillies = "family10"  # validation images, list families to separate train, test
 
 all_images = glob(train_folders_path + '/*/*/*.jpg')
@@ -39,12 +39,13 @@ for x in val_images:
 # get pairs from csv to a zipped list
 relationships = pd.read_csv(train_file_path)
 relationships = list(zip(relationships.p1.values, relationships.p2.values))
-# validate for people - check after if all accounted for
+# validate for people
 relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
 
 # get train and val relationship list
 train = [x for x in relationships if val_famillies not in x[0]]
 val = [x for x in relationships if val_famillies in x[0]]
+
 
 # print("checkpoint") #for debugging
 
@@ -52,7 +53,8 @@ val = [x for x in relationships if val_famillies in x[0]]
 # read images
 def read_img(path):
     img = cv2.imread(path)
-    img = np.array(img).astype(np.float)
+    img = cv2.resize(img, (224, 224))
+    img = np.array(img, dtype="float64")
     return preprocess_input(img, version=2)
 
 
@@ -60,7 +62,7 @@ def read_img(path):
 def gen(list_tuples, person_to_images_map, batch_size=16):
     ppl = list(person_to_images_map.keys())
     while True:
-        batch_tuples = sample(list_tuples, batch_size // 2) # random tuples
+        batch_tuples = sample(list_tuples, batch_size // 2)  # random tuples
         labels = [1] * len(batch_tuples)
         while len(batch_tuples) < batch_size:
             # choose 2 people
@@ -84,6 +86,8 @@ def gen(list_tuples, person_to_images_map, batch_size=16):
         X2 = [choice(person_to_images_map[x[1]]) for x in batch_tuples]
         X2 = np.array([read_img(x) for x in X2])
 
+        labels = np.array(labels)
+
         yield [X1, X2], labels
 
 
@@ -92,7 +96,7 @@ def baseline_model():
     input_1 = Input(shape=(224, 224, 3))
     input_2 = Input(shape=(224, 224, 3))
 
-    base_model = VGGFace(model='resnet50', include_top=False)
+    base_model = VGGFace(model='vgg16', include_top=False)
 
     for x in base_model.layers[:-3]:
         x.trainable = True
@@ -102,7 +106,6 @@ def baseline_model():
 
     # x1_ = Reshape(target_shape=(7*7, 2048))(x1)
     # x2_ = Reshape(target_shape=(7*7, 2048))(x2)
-    #
     # x_dot = Dot(axes=[2, 2], normalize=True)([x1_, x2_])
     # x_dot = Flatten()(x_dot)
 
@@ -124,7 +127,7 @@ def baseline_model():
     return model
 
 
-file_path = "vgg_face.h5"
+file_path = "D:/Files on Desktop/engine/fax/magistrska naloga/vgg_face.h5"
 
 # callback to save weights
 checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
@@ -133,13 +136,15 @@ checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_
 reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.1, patience=20, verbose=1)
 
 callbacks_list = [checkpoint, reduce_on_plateau]
+# callbacks_list = [reduce_on_plateau]
 
 # can load weights below
 model = baseline_model()
+
 # model.load_weights(file_path)
-model.fit_generator(gen(train, train_person_to_images_map, batch_size=16), use_multiprocessing=True,
-                    validation_data=gen(val, val_person_to_images_map, batch_size=16), epochs=100, verbose=2,
-                    workers=4, callbacks=callbacks_list, steps_per_epoch=200, validation_steps=100)
+model.fit(gen(train, train_person_to_images_map, batch_size=16), use_multiprocessing=True,
+          validation_data=gen(val, val_person_to_images_map, batch_size=5), epochs=100, verbose=2,
+          workers=4, callbacks=callbacks_list, steps_per_epoch=200, validation_steps=100)
 
 test_path = "../input/test/"
 
