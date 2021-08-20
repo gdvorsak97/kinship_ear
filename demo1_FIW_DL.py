a9f61194da07ba35a1879ab5f5df5d1d525b2549
Grega Dvoršak <gdvorsak97@gmail.com>
2021-08-20 18:38:27 +0200

    Modifications to vgg, got best ROC

--- demo1_FIW_DL.py
+++ demo1_FIW_DL.py
@@ -6,6 +6,9 @@ import pandas as pd
 import cv2
 import numpy as np
 from random import choice, sample
+
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Rescaling
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
@@ -23,12 +26,43 @@ mpl.rcParams['figure.figsize'] = (12, 10)
 
 train_file_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/train_list.csv"
 train_folders_path = "D:/Files on Desktop/engine/fax/magistrska naloga/Ankitas Ears/train/"
-val_famillies = "family10"  # validation images, list families to separate train, test
+val_famillies = ["family10", "family4"]  # validation images, list families to separate train, test
 
 all_images = glob(train_folders_path + '/*/*/*.jpg')
 all_images = [x.replace("\\", "/") for x in all_images]
-train_images = [x for x in all_images if val_famillies not in x]  # all except families in val_families
-val_images = [x for x in all_images if val_famillies in x]  # all images not in validation set
+all_files = [str(i).split("/")[-1][:-4] for i in all_images]
+
+# Filter step fOr bounding boxes
+delete_path = "D:\\Files on Desktop\\engine\\fax\\magistrska naloga\\Ankitas Ears\\bounding boxes alligment" \
+              "\\delete list.txt"
+delete_file = pd.read_csv(delete_path, delimiter=";")
+FILTERS = "maj_oob,mnr_oob,blr,ilu,drk,grn,lbl"
+filters = FILTERS.replace(" ", "")
+filters = filters.split(",")
+deleted = []
+for i in all_files:
+    check = delete_file["filename"] == i
+    check = list(np.where(check)[0])
+    if len(check) != 0:
+        d_fs = delete_file["filter"].iloc[check]
+        current_f = list(d_fs.values)[0].split(',')
+        for f in current_f:
+            if f in filters:
+                to_delete = all_files.index(i)
+                deleted.append(to_delete)
+                break
+
+# delete filtered from all images
+all_images = [all_images[i] for i in range(len(all_images)) if i not in deleted]
+
+train_images = []
+val_images = []
+for x in all_images:
+    for i in range(len(val_famillies)):
+        if val_famillies[i] not in x:
+            train_images.append(x)
+        elif val_famillies[i] in x:
+            val_images.append(x)
 
 # dictionary for a map of person:image
 train_person_to_images_map = defaultdict(list)
@@ -50,8 +84,16 @@ relationships = list(zip(relationships.p1.values, relationships.p2.values))
 relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
 
 # get train and val relationship list
-train = [x for x in relationships if val_famillies not in x[0]]
-val = [x for x in relationships if val_famillies in x[0]]
+train = []
+val = []
+
+for i in range(len(val_famillies)):
+    for x in relationships:
+        if val_famillies[i] not in x[0]:
+            train.append(x)
+        elif val_famillies[i] in x[0]:
+            val.append(x)
+
 
 METRICS = [
     keras.metrics.TruePositives(name='tp'),
@@ -100,12 +142,75 @@ def plot_metrics(history):
     plt.show()
 
 
+def visualize_crop(in_img, crp_img):
+    in_img = cv2.cvtColor(in_img, cv2.COLOR_BGR2RGB)
+    crp_img = cv2.cvtColor(crp_img, cv2.COLOR_BGR2RGB)
+    fig, axes = plt.subplots(1, 2)
+    axes[0].imshow(in_img[:, :, :])
+    axes[0].set_title('Original image')
+    axes[1].imshow(crp_img[:, :, :])
+    axes[1].set_title('Cropped input')
+    fig.suptitle(f'Original and cropped input')
+    fig.set_size_inches(9, 5, forward=True)
+    plt.show()
+
+
+def alignment(image, path, visualize=False, save=False):
+
+    label_path = "D:\\Files on Desktop\\engine\\fax\\magistrska naloga\\Ankitas Ears\\bounding boxes alligment\\"
+    delete_ist_path = label_path + "delete list.txt"
+    filename = ""
+    if "/" in path:
+        family = path.split("/")[-3]
+        filename = path.split("/")[-1]
+        label_path += "labels " + str(family) + ".csv"
+    elif "\\" in path:
+        filename = path.split("\\")[-1]
+        label_path = label_path + "all_labels.csv"
+    label_file = pd.read_csv(label_path)
+    bbox_data = label_file[label_file['file'] == filename]
+    bbox = image[bbox_data['y1'].values[0]:bbox_data['y1'].values[0] + bbox_data['dy'].values[0],
+           bbox_data['x1'].values[0]:bbox_data['x1'].values[0] + bbox_data['dx'].values[0]]
+    if visualize:
+        cv2.imshow("Detected", bbox)
+        cv2.waitKey()
+    if save:
+        cv2.imwrite("example.png", bbox)
+    return bbox
+
+
+def crop_ears(img, region):
+    if region == "left":
+        img = img[:, 0:int(np.round(224 / 3))]
+    elif region == "right":
+        img = img[:, -int(np.round(224 / 3)):]
+    elif region == "mid_vertical":
+        img = img[:, int(np.round(224 / 6)):int(np.round(224 / 6)) + int(np.round(224 / 3))]
+    elif region == "top":
+        img = img[0:int(np.round(224 / 3)), :]
+    elif region == "mid_horizontal":
+        img = img[int(np.round(224 / 6)):int(np.round(224 / 6)) + int(np.round(224 / 3)), :]
+    elif region == "bottom":
+        img = img[-int(np.round(224 / 3)):, :]
+    return img
+
+
+rescale = Sequential([
+    Rescaling(1. / 255)
+])
+
+
 # read images
 def read_img(path):
-    img = cv2.imread(path)
-    img = cv2.resize(img, (224, 224))
+    in_img = cv2.imread(path)
+    in_img = alignment(in_img, path)
+    in_img = cv2.resize(in_img, (224, 224))
+    # img = crop_ears(in_img, "top")
+    img = cv2.resize(in_img, (224, 224))
     img = np.array(img, dtype="float64")
-    return preprocess_input(img, version=1)  # 1 for VGG, 2 otherwise
+    img = preprocess_input(img, version=1)  # 1 for VGG, 2 otherwise
+    img = rescale(img)
+    return img
 
 
 # generator of labels for pairs
@@ -181,11 +286,11 @@ def baseline_model():
     return model
 
 
-n_epochs = 25
-n_steps_per_epoch = 30
-n_val_steps = 10
+n_epochs = 40
+n_steps_per_epoch = 100
+n_val_steps = 32
 key = "031109_" + str(n_epochs) + "_" + str(n_steps_per_epoch) + "_" + str(n_val_steps)
-file_path = "D:/Files on Desktop/engine/fax/magistrska naloga/vgg_face_" + key + ".h5"
+file_path = "vgg_face_best.h5"
 
 # callback to save weights
 checkpoint = ModelCheckpoint(file_path, verbose=1, save_best_only=True)
@@ -199,9 +304,8 @@ callbacks_list = [checkpoint, reduce_on_plateau]
 model = baseline_model()
 
 # model.load_weights(file_path)
-baseline_history = model.fit(gen(train, train_person_to_images_map, batch_size=16), use_multiprocessing=False,
+baseline_history = model.fit(gen(train, train_person_to_images_map, batch_size=8), use_multiprocessing=False,
                              validation_data=gen(val, val_person_to_images_map, batch_size=5), epochs=n_epochs,
-                             verbose=2,
                              workers=1, callbacks=callbacks_list, steps_per_epoch=n_steps_per_epoch,
                              validation_steps=n_val_steps)
 
@@ -234,5 +338,5 @@ for batch in tqdm(chunker(results.img_pair.values)):
 # output
 results['is_related'] = predictions
 
-results.to_csv("vgg_face_results" + key + ".csv", index=False)
+results.to_csv("vgg_face_results.csv", index=False)
 
